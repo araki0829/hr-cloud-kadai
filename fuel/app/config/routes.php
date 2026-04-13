9c10caa232e15fe5a20c1e86f8a419f953f663d6
Araki <davidsizu5051r2@gmail.com>
2026-04-13 17:39:10 +0900

    ルーティング定義をした

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,7 +1,25 @@
 <?php
 return array(
-	'_root_'  => 'welcome/index',  // The default route
-	'_404_'   => 'welcome/404',    // The main 404 route
-	
-	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+	'_root_'  => 'auth/login',
+	'_404_'   => 'welcome/404',
+
+	// auth
+	'login' => 'auth/login',
+	'signup' => 'auth/signup',
+	'logout' => 'auth/logout',
+
+	// projects
+	'projects' => 'projects/index',
+	'projects/create' => 'projects/create',
+	'projects/edit/(:num)' => 'projects/edit/$1',
+	'projects/delete/(:num)' => 'projects/delete/$1',
+
+	// tasks
+	'projects/(:num)/tasks' => 'tasks/index/$1',
+	'projects/(:num)/tasks/create' => 'tasks/create/$1',
+	'tasks/edit/(:num)' => 'tasks/edit/$1',
+	'tasks/delete/(:num)' => 'tasks/delete/$1',
+
+	// api
+	'api/tasks/change_status' => 'api/tasks/change_status',
 );
