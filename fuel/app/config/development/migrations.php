d1df77bfb52803641d201b19e0284380ec7aaff5
Araki <davidsizu5051r2@gmail.com>
2026-04-14 06:40:26 +0900

    tasks.statusに0・1・2のみ許可するCHECK制約を追加

--- fuel/app/config/development/migrations.php
+++ fuel/app/config/development/migrations.php
@@ -9,6 +9,7 @@ return array(
         0 => '001_create_users',
         1 => '002_create_projects',
         2 => '003_create_tasks',
+        3 => '004_add_check_constraint_to_tasks_status',
       ),
     ),
     'module' => 
